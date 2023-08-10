48bea77a7e24bdf51aedac6ceced4e3d9aadaed1
roniekun <roniebenitez01@gmail.com>
2023-08-10 19:30:31 +0800

    gsap start

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,26 +4,45 @@ import { ThemeContext } from '../context/ThemeContext';
 import SocialLinks from './pages/assets/icons/SocialLinks';
 import SiteLogo from './SiteLogo';
 import RequestBtn from './pages/assets/buttons/RequestBtn';
+import React, { useEffect, useRef } from 'react';
+import gsap from 'gsap';
+import { delay } from 'framer-motion';
 
 
 const Navbar = ({ showNavbar, isSmallScreen, isMediumScreen, setShowNavbar, isDesktop, isScroll }) => {
   const location = useLocation();
-
+  const navbarContainerRef = useRef(null);
+  const navbarlinksRef = useRef(null);
 
   const links = [
     { to: '/', text: 'home' },
     { to: '/about', text: 'about' },
     { to: '/portfolio', text: 'work' },
-    // { to: '/blog', text: 'blog' },
     { to: '/contact', text: 'contact' },
-    
   ];
+
+  useEffect(() => {
+    if (showNavbar) {
+      gsap.to(navbarContainerRef.current, { y: '0%', duration: 0.5, opacity: 1, ease: 'power2.out' });
+    } else {
+      gsap.to(navbarContainerRef.current, { y: '-100%', duration: 0.3, opacity: 0.5, ease: 'power2.in' });
+    }
+  }, [showNavbar]);
+
+  useEffect(() => {
+    if (showNavbar) {
+      gsap.fromTo(
+        navbarlinksRef.current,
+        { y: '70px', opacity: 0 },
+        { y: '0', opacity: 1, duration: 0.5, }
+      );
+    }
+  }, [showNavbar]);
 
   const handleLinkClick = () => {
 
     window.scrollTo({ top: 0 })
     setShowNavbar(!isSmallScreen);
-    console.log('Link clicked and active');
   };
 
   return (
@@ -31,22 +50,24 @@ const Navbar = ({ showNavbar, isSmallScreen, isMediumScreen, setShowNavbar, isDe
       <ThemeContext.Consumer>
         {(themeContext) => (
           <nav 
-          className={`navbar_container ${isScroll ? 'bg-color' : ''} ${showNavbar ? '' : 'hide'}`}
+          ref={navbarContainerRef} 
+          className={`navbar_container ${isScroll ? 'bg-color' : ''}`}
           id={`component-${themeContext.theme}`}>
             <>
           { isSmallScreen && <div className='nav_row_title'> <h4 >Sections</h4> </div>} 
-            
+          
             <>
            {isDesktop && <SiteLogo showNavbar={showNavbar} 
                                     navbarContainer={{ color : '#3D3D3d', width: '15vw'}} />}
-            <div className="links_container">
+            <div className="links_container"  ref={navbarlinksRef}>
+              
 
               {links.map((link) => (
                 <div
                   className="link_wrapper"
-                  key={link.to}
+                  key={link.to}  
                 >
-                  <NavLink
+                  <NavLink 
                     onClick={() => 
                     handleLinkClick(location.pathname === link.to)}
                     className={`navbar_link ${location.pathname === link.to ? 'active_link' : ''}`}
@@ -58,7 +79,6 @@ const Navbar = ({ showNavbar, isSmallScreen, isMediumScreen, setShowNavbar, isDe
               ))}
             </div></>
             </>
-            {/* {isDesktop && <Search showNavbar={showNavbar } color={ 'white'}/>} */}
             {!isMediumScreen && !isSmallScreen && 
             <RequestBtn isDesktop={isDesktop}
                        navbarButton={{border:'none',
@@ -69,8 +89,6 @@ const Navbar = ({ showNavbar, isSmallScreen, isMediumScreen, setShowNavbar, isDe
                                       width: '15vw'}}
                         navbarSvg={{border: 'black solid 1px'}}/>
                                        }
-                       
-            
             {isSmallScreen &&
             <>
           <div className='nav_row_title'><h4>Connect</h4></div>
@@ -91,7 +109,6 @@ const Navbar = ({ showNavbar, isSmallScreen, isMediumScreen, setShowNavbar, isDe
           }}
               displayNames={true}
               showNavbar={showNavbar}/>
-
             </>
            }
            <br />
@@ -99,7 +116,7 @@ const Navbar = ({ showNavbar, isSmallScreen, isMediumScreen, setShowNavbar, isDe
             {isSmallScreen && (<div className='nav_email'>  <h4>Email </h4>
             <h3>roniebenitez01@gmail.com</h3></div>
            ) }
-            
+        
           </nav>
         )}
       </ThemeContext.Consumer>
